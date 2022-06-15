a49d8329292eb0c85f74cf323f4b5a7718d1fd7a
ghospadi <Lolr2223@gmail.com>
2022-06-15 16:03:04 +0300

    init

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
